18973d94bf7d62d3b070da7d09ba0f2ee121fd2d
Ramiz khan <ramizkhan@Ramizs-MacBook-Pro-2.local>
2017-09-20 21:15:48 -0500

    hopefully this goed the right place

--- api.js
+++ api.js
@@ -1,7 +1,10 @@
 console.log("inside js 2")
 
+$(document).ready(function(){
 
-var queryURL = "https://api.unsplash.com/search/photos/?client_id=8282b5778d15130d8b4252e3f590c0d5bf52f33c545e6467bdd3ba7efbce1b83&query=deep+space";
+var queryURL = "https://api.unsplash.com/collections/142563/photos/?client_id=8282b5778d15130d8b4252e3f590c0d5bf52f33c545e6467bdd3ba7efbce1b83&query=outer+space+stars";
+//var queryURL = "https://api.unsplash.com/photos/random/?client_id=8282b5778d15130d8b4252e3f590c0d5bf52f33c545e6467bdd3ba7efbce1b83&query=deep+space";
+
 
 $.ajax({
 	url : queryURL,
@@ -9,9 +12,20 @@ $.ajax({
 	})
 .done(function(response) {
 
-	var imgURL = (response.results[0].urls.full);
+	var imgURL = (response[0].urls.full);
 
-		console.log(response.results[0].urls.full);
+		console.log(response);
 		$("body").css("background-image", "url("+imgURL+")");
 
 		});
+
+
+});
+
+ 
+
+
+
+
+
+  
